d2082066091323e86fe8d8e0a46c0037122b04ef
kaylaneuy <kaylane2anjos.designer@gmail.com>
2025-01-03 18:29:06 -0300

    constroi footer
    Co-authored-by: Gustavo de Albuquerque Ramalho <gramalho.ntt@rededor.com.br>

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,12 +1,44 @@
 class MobileNavbar {
-  constructor () {
-    this.mobilemenu = document.querySelector (mobilemenu);
-    this.navlist = document.querySelector (navlist);
-    this.navlinks = document.querySelectorAll (navlinks);
+  constructor(mobilemenu, navlist, navlinks) {
+    this.mobilemenu = document.querySelector(mobilemenu);
+    this.navlist = document.querySelector(navlist);
+    this.navlinks = document.querySelectorAll(navlinks);
     this.activeClass = "active";
+
+    this.handleClik = this.handleClik.bind(this);
+
   }
 
-  addClickEvent () {
-    this.mobilemenu.addEventListener("click", () => console.log())
+  animateLinks() {
+    this.navlinks.forEach((link, index) => {
+      link.style.animation
+      ? (link.style.animation = "")
+      : (link.style.animation = `navLinkFade 0.5 ease forwards $ { indez / 7 + 0.3}s`)
+    });
+  }
+
+  handleClik () {
+    this.navlist.classList.toggle(this.activeClass);
+    this.mobilemenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobilemenu.addEventListener("click", this.handleClik);
+  }
+
+  init() {
+    console.log(this.mobilemenu)
+    if (this.mobilemenu) {
+      this.addClickEvent();
+    }
+    return this;
   }
 }
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
